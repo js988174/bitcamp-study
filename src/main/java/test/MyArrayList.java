f3f00bbb80192f0479f5d369f1b7c8e2cd6a1108
js988174 <rudtjs4902@naver.com>
2021-02-14 16:53:54 +0900

    .

--- src/main/java/test/MyArrayList.java
+++ src/main/java/test/MyArrayList.java
@@ -5,11 +5,29 @@ public class MyArrayList {
   static int size;
 
   static public boolean add(Object e) {
+    if (size == elementDate.length) {
+      grow();
+    }
     elementDate[size++] = e;
     return true;
   }
 
+  static private void grow() {
+    System.out.println("배열 늘리기");
+    Object[] newArray = new Object[elementDate.length + (elementDate.length >>1)];
+    for (int i = 0; i < elementDate.length; i++) {
+      newArray[i] = elementDate[i];
+    }
+    elementDate = newArray;
+  }
+
   static public void add (int index, Object element) {
+    if (size == elementDate.length) {
+      grow();
+    }
+    if (index < 0 || index > size) {
+      throw new ArrayIndexOutOfBoundsException("인덱스 x");
+    }
     for (int i = size; i > index; i--) {
       elementDate[i] = elementDate[i-1];
     }
